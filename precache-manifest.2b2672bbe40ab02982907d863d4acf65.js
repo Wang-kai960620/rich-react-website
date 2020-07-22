1bc6866991937f27b5abd91ee8c611fd9e441c37
wangkai <wk.dong.up@gmail.com>
2020-07-22 11:02:10 +0800

    deploy

---
+++ precache-manifest.2b2672bbe40ab02982907d863d4acf65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbbab2eee377b9d1e4eb2df342e6c9e",
+    "url": "/rich-react-website/index.html"
+  },
+  {
+    "revision": "fe1d41cf0e4929c79264",
+    "url": "/rich-react-website/static/css/main.a1963f6b.chunk.css"
+  },
+  {
+    "revision": "09c95d38b8ff32dd0f55",
+    "url": "/rich-react-website/static/js/2.70ef7295.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/rich-react-website/static/js/2.70ef7295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1d41cf0e4929c79264",
+    "url": "/rich-react-website/static/js/main.b9ff4bab.chunk.js"
+  },
+  {
+    "revision": "42701eda8f82932d3f3d",
+    "url": "/rich-react-website/static/js/runtime-main.a651c89f.js"
+  }
+]);
